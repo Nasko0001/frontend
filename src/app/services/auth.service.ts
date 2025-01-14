3831345a5d294b32148d6d8001573513421d7c23
En-nasery Abdennacer <153748801+Nasko0001@users.noreply.github.com>
2025-01-14 12:22:51 +0100

    Initial commit

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {jwtDecode} from "jwt-decode";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  isAuthenticated: boolean = false;
+  roles : any;
+  usename : any;
+  accessToken! : string;
+
+  constructor(private http:HttpClient) { }
+
+  public login(username : string, password: string) {
+    let options = {
+      headers : new HttpHeaders().set("Content-Type", "application/x-www-form-urlencoded")
+    }
+    let params = new HttpParams().set("username", username).set("password", password);
+    return this.http.post("http://localhost:8085/auth/login",params,options)
+  }
+  loadProfile(data: any){
+    this.isAuthenticated = true;
+    this.accessToken = data['access_token'];
+    let decodedJwt:any = jwtDecode(this.accessToken);
+    this.usename=decodedJwt.sub;
+    this.roles = decodedJwt.scope;
+    console.log(this.roles);
+  }
+}
